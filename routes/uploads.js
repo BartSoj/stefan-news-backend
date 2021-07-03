2d21af0f7319db671da0359b03f6d215f02971e0
Bartosz <bartsoj@gmail.com>
2021-07-03 20:24:04 +0200

    change upload photos destination to google cloud storage

--- routes/uploads.js
+++ routes/uploads.js
@@ -2,28 +2,43 @@ const router = require("express").Router();
 const multer = require("multer");
 const path = require("path");
 const auth = require("../middleware/auth");
-const fs = require("fs");
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "uploads");
+const {Storage} = require("@google-cloud/storage");
+const storage = new Storage();
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 5 * 1024 * 1024,
     },
-    filename: (req, file, cb) => {
-        const filename = Date.now() + path.extname(file.originalname);
-        cb(null, filename);
+});
+
+const bucket = storage.bucket(process.env.BUCKET_NAME);
+
+router.route("/").post(auth, upload.single("image"), async (req, res, next) => {
+    if (!req.file) {
+        return res.status(400).send({"error": "brak pliku"});
     }
-});
 
-const upload = multer({storage: storage});
-
-router.route("/").post(auth, upload.single("image"), (req, res) => {
-    return res.json(req.file.filename);
-});
-
-router.route("/:filename").delete(auth, (req, res) => {
-    fs.unlink("uploads/" + req.params.filename, err => {
-        err ? res.status(400).json(err) : res.json(true);
+    const blob = bucket.file(Date.now() + path.extname(req.file.originalname));
+    const blobStream = blob.createWriteStream({
+        resumable: false,
     });
+
+    blobStream.on("error", err => {
+        next(err);
+    });
+
+    blobStream.on("finish", () => {
+        res.json(blob.name);
+    });
+
+    blobStream.end(req.file.buffer);
+});
+
+router.route("/:filename").delete(auth, async (req, res) => {
+    await bucket.file(req.params.filename).delete();
+    res.json(true);
 });
 
 module.exports = router;
